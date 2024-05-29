9c3d1009b755f7212b0e670c1760fb6d7375b139
Anmol Gangwar <anmolgangwar64@gmail.com>
2024-05-29 07:49:36 +0530

    chore :zap: update user model user bio

--- src/infra/mongodb/models/users/user.schema.ts
+++ src/infra/mongodb/models/users/user.schema.ts
@@ -98,6 +98,7 @@ const userSchema = new mongoose.Schema<IUserDoc, IUserModel>(
     bio: {
       type: String,
       trim: true,
+      default: null,
     },
     resetToken: {
       type: String,
